4d84ee867dd9c14d5cb6c71ffd6e08e118445484
sangmin han <sangriel@naver.com>
2024-03-10 22:18:33 +0900

    HTTP요청 기본과 헤더 조회

--- src/main/java/hello/springmvc/basic/requestmapping/MappingClassController.java
+++ src/main/java/hello/springmvc/basic/requestmapping/MappingClassController.java
@@ -30,7 +30,7 @@ public class MappingClassController {
     @DeleteMapping("/{userId}")
     public String deleteUser(@PathVariable String userId) {
         return "delete userId=" + userId;
-    } 
+    }
 
 
 }
